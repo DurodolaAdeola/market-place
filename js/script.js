3352e44aaf8f5694e90001e60acbc77e6a1f8e36
adeola <durodolaadeola7@gmail.com>
2021-11-05 16:55:07 +0100

    home

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
     $("#fish").click(function(){
-        window.location.href="reciept.html"
+        window.location.href="form.html"
     });
     $("#food").click(function(){
-        window.location.href="reciept.html"
+        window.location.href="form.html"
     });
     $("#rice").click(function(){
-        window.location.href="reciept.html"
+        window.location.href="form.html"
     });
-    $("#chick").click(function(){
-        window.location.href="reciept.html"
+    $("#chic").click(function(){
+        window.location.href="form.html"
     });
-    $("#fish").click(function(){
-        window.location.href="reciept.html"
+    $("#meat").click(function(){
+        window.location.href="form.html"
     });
-    $("#fish").click(function(){
-        window.location.href="reciept.html"
+    $("#egg").click(function(){
+        window.location.href="form.html"
+    });
+    $("#beans").click(function(){
+        window.location.href="form.html"
+    });
+    $("#yam").click(function(){
+        window.location.href="form.html"
+    });
+        $("#btn").click(function(){
+        window.location.href="index.html"
     });
 
 
