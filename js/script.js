9eb293a686e8538a18ef673617c55a984012381d
adeola <durodolaadeola7@gmail.com>
2021-11-05 15:29:47 +0100

    marketplace

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#fish").click(function(){
+        window.location.href="reciept.html"
+    });
+    $("#food").click(function(){
+        window.location.href="reciept.html"
+    });
+    $("#rice").click(function(){
+        window.location.href="reciept.html"
+    });
+    $("#chick").click(function(){
+        window.location.href="reciept.html"
+    });
+    $("#fish").click(function(){
+        window.location.href="reciept.html"
+    });
+    $("#fish").click(function(){
+        window.location.href="reciept.html"
+    });
+
+
+
+
+
+
+})
