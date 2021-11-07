aeac246690c8b305f258e77cb813ba59bdcc0893
adeola <durodolaadeola7@gmail.com>
2021-11-07 12:48:36 +0100

    form

--- js/script.js
+++ js/script.js
@@ -26,10 +26,24 @@ $(document).ready(function(){
         $("#btn").click(function(){
         window.location.href="index.html"
     });
+  
 
 
+})
+$(document).ready(function(){
+    ("form#form").submit(function(event){
+        
+        let car =($("#car").val());
+        let firstName =($("#firstname").val());
+        let lastName =($("#lastname").val());
+        let middleName =($("#middlename").val());
+        event.preventDefault();
+        
+    
+    
+    $("#output").text(`${firstName} ${lastName} ${middleName} ${car}`);
 
-
-
+    })
+     
 
 })
